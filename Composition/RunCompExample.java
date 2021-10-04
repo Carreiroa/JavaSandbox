bb7e3d131499cde5e60b92ff6c2414d897be12c7
Carreiroa <77595798+Carreiroa@users.noreply.github.com>
2021-10-04 11:27:59 -0400

    Final version of this example

--- Composition/RunCompExample.java
+++ Composition/RunCompExample.java
@@ -1,25 +1,62 @@
-class RunCompExample {
-	public static void main (String [] args){
-	
-		Contact [] myContacts = new Contact [4];
-		PhoneBook myPhoneBook =new PhoneBook(myContacts);
+import java.util.ArrayList;
 
-		myPhoneBook.display();
-	}
+class RunCompExample {
+   public static void main (String [] args){
+   
+      Contact [] myContacts = new Contact [4];
+      PhoneBook myPhoneBook;
+      
+      myContacts[0] = new Contact ("Andy","Carreiro","905-555-1234");
+      myContacts[1] = new Contact ("Julie","Carreiro","905-555-1234");
+      myContacts[2] = new Contact ("Gabe","Carreiro","905-555-1234");
+      myContacts[3] = new Contact ("Elizabeth","Carreiro","905-555-1234");
+      
+      myPhoneBook = new PhoneBook(myContacts);
+      myPhoneBook.displayContacts();
+   }
 }
 
 class Contact {
-	String  fName;
-	String lName;
-	String phNum;
+   String  fName;
+   String lName;
+   String phNum;
 
-	Contact (String f, String l, String p) {
-		fName = f;
-		lName =l;
-		phNum=p;
-	}	
+   Contact (String f, String l, String p) {
+      fName = f;
+      lName =l;
+      phNum=p;
+   }	
 }
 
 class PhoneBook {
+
+   ArrayList <Contact> contacts = new ArrayList<>();
+
+   PhoneBook (Contact [] contacts) {
+   
+      for (Contact contact : contacts ) {
+      
+         this.contacts.add(contact);
+      
+      }
+   }
+
+   void displayContacts (){
+   
+      if (contacts.get(0) != null) {
+      
+         System.out.println("---------------- Contacts ----------------");
+         System.out.printf ("\n%-15s%-15s%-15s\n\n","First Name","Last Name","Phone Number");
+         
+         for (Contact contact: this.contacts){
+            System.out.printf ("%-15s%-15s%-15s\n",contact.fName,contact.lName,contact.phNum);
+         }
+         
+      }else{
+         System.out.print("You don't have any contacts in your address book.");
+      }
+   }
+
+
 
 }
